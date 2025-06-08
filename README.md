b183e46e5f106cd8d20df7fe8909f13a3cf86e32
msk2k1 <primary686@gmail.com>
2025-06-07 21:06:59 -0400

    commandline args for roots

--- README.md
+++ README.md
@@ -1,31 +1,40 @@
-# comptool
-
 A platform-agnostic tool for comparing an internal drive to an external copy.
 
-### Usage
+# Usage
 
 Run from the command line: `python <folder>/comptool.py <args>`
 
 By default (when passing no parameters), the script will attempt to compare everything in the two roots.
 The folders that will be compared will be previewed before comparison is attempted.
 
-**Arguments:**
-`-h`, `--help`                  show this help message and exit
-`-s`, `--subfolder` SUBFOLDER   Path to a subfolder to compare.
+## Arguments
+- `-h` or `--help`: show this help message and exit
+- `-s` or `--subfolder`: Path to a subfolder to compare, as opposed to the entirety of the drives.
+    - Provide a path to the folder on one of the drive copies. The script attempts to extract the subfolder path from this and apply it to both roots. 
+- `-l` or `--left-root`: Path to root of filesystem on left side of comparison.
+- `-r` or `--right-root` or `--rite-root`: Path to root of filesystem on right side of comparison.
+- `-n` or `--no-log`: Disable logging to output file.
 
-**Return codes:**
+## Return Codes
 - 0 => success (no errors encountered)
 - 1 => error during comparison
-- 2 => error with input parameters
-- 3 => error parsing config file
+- 2 => error parsing input parameters
 
-**Dependencies:** linux: diff and meld; windows: winmerge
+## Dependencies 
+diff and meld on Linux; Winmerge on Windows
 
-### Configuration
+<!-- ----------------------------------------------------- -->
 
-Define roots of internal data drive, and where external data drive is mounted, in `comptool.config`. This file should be in the **same folder** as the script.
+# Configuration
 
-The config file can have unique entries for different computers by name. Here is an example entry:
+## Config File
+Various settings can be pre-defined per machine in `comptool.config`, located in the **same folder** as the script:
+- `left` and `rite`: the roots of the two file systems to compare.
+- `logfile`: where to log comparisons when they are made on this machine.
+- `ignores`: a string to represent files and folders to exclude from comparison. Separate entries with comma (`,`). End folders with double backslash (`\\`).
+- `winmenge`: for windows machines, the path to the WinMerge executable. Use double backslashes in path.
+
+Here is an example entry. `MSK-PC` is the name of the computer for which this configuration applies.
 ```
 [MSK-PC]
 left = F:\
@@ -35,14 +44,18 @@ ignores = My*\\,desktop.ini,WindowsPowershell\\,Zoom\\
 winmerge = C:\\Program Files\\WinMerge\\WinMergeU.exe
 ```
 
-Configuration options are:
-- `left` and `rite`: the roots of the two file systems to compare.
-- `logfile`: where to log comparisons when they are made on this machine.
-- `ignores`: a string to represent files and folders to exclude from comparison. Separate entries with comma (`,`). End folders with double backslash (`\\`).
-- `winmenge`: for windows machines, the path to the WinMerge executable. Use double backslashes in path.
+A configuration must at least include the left and right directories. The other options are only used if they are included (logfile, ignores) or necessary (winmerge, for Windows machines).
 
-### Logs
+## Commandline Parameters
+If provided, commandline parameters `--left-root` and `--rite-root` take priority over the config file.
+This allows you to temporarily override one of them for a single comparison, instead of needing to edit the config file.
+
+<!-- ----------------------------------------------------- -->
+
+# Logs
 
 Note that as written, the log file will only show the paths relative to the roots being used on that system.
 This means an overall comparison (w/out specifying subfolder) will have just "/" for these runs.
 You will need to know the left and right roots used at the time to deduce the full paths.
+
+Logging can be disabled with the `--no-log` flag (see "Usage" section).
--- comptool.py
+++ comptool.py
@@ -1,16 +1,5 @@
 import os, subprocess, argparse, sys, datetime, configparser, platform
 
-# get folder paths from config file
-try:
-    config = configparser.ConfigParser()
-    config.read(f'{os.path.dirname(os.path.realpath(__file__))}/comptool.config') # config file name
-    section = platform.node()
-    LEFT_ROOT = config[section]['left']
-    RITE_ROOT = config[section]['rite']
-    LOG_FILE = config[section]['logfile']
-    IGNORES = config[section]['ignores'].split(',')
-except Exception as e: print(f"Could not read configuration: {e}"); sys.exit(3)
-
 def choice(prompt: str):
     print(prompt, end=' ')
     while True:
@@ -53,27 +42,69 @@ def windows_compare():
 
 ############################# MAIN EXECUTION STARTS HERE #############################
 
-# comparison prerequisites
+# read config file
 try:
-    # get commandline arguments
+    config = configparser.ConfigParser()
+    config.read(f'{os.path.dirname(os.path.realpath(__file__))}/comptool.config') # config file name
+    section = platform.node() # section title = computer name
+    if section not in config: print("Preset configuration not found, reading root paths from commandline")
+except Exception as e: print(f"Could not access configuration file: {e}")
+
+# get commandline arguments
+try:
     parser = argparse.ArgumentParser(prog='comptool', description='A platform-agnostic tool for comparing an internal drive to an external copy.')
     parser.add_argument('-s', '--subfolder', help='Path to a subfolder to compare.')
+    parser.add_argument('-l', '--left-root', help='Path to root of filesystem on left side of comparison. Overrides definition from config file if exists.')
+    parser.add_argument('-r', '--rite-root', '--right-root', help='Path to root of filesystem on right side of comparison. Overrides definition from config file if exists.')
+    parser.add_argument('-n', '--no-log', help='Disable logging to output file.', action='store_true')
     ARGS = parser.parse_args()
+except Exception as e: print(f"Input parameters error: {e}"); sys.exit(2)
 
+# define constants
+try:
+
+    if ARGS.left_root: LEFT_ROOT = ARGS.left_root
+    else:
+        try: LEFT_ROOT = config[section]['left']
+        except: raise Exception("Left directory must be defined.")
+
+    if ARGS.rite_root: RITE_ROOT = ARGS.rite_root
+    else:
+        try: RITE_ROOT = config[section]['rite']
+        except: raise Exception("Right directory must be defined.")
+    
+    try: 
+        if ARGS.no_log: raise Exception
+        LOG_FILE = config[section]['logfile']
+        if not os.path.exists(os.path.dirname(LOG_FILE)): 
+            print("Folder for logfile does not exist; logging will not be performed.")
+            raise Exception
+    except: LOG_FILE = None
+    
+    try: IGNORES = config[section]['ignores'].split(',')
+    except: 
+        print("Ignore files/directories configuration not detected or is malformed. Nothing will be ignored.")
+        IGNORES = []
+
+except Exception as e: print(f"Error collecting arguments: {e}"); sys.exit(2)
+
+# subfolder (define ITEM constant)
+try:
     # extract just item to compare from given path (define ITEM constant)
     # if subfolder is provided, strip the containing root from the path so it can be added to both roots.
-    if ARGS.subfolder == None: ITEM = ""
-    else:
+    if ARGS.subfolder:
         ITEM = os.path.realpath(ARGS.subfolder)
         if   ITEM.startswith(LEFT_ROOT): ITEM = ITEM[len(LEFT_ROOT):]
         elif ITEM.startswith(RITE_ROOT): ITEM = ITEM[len(RITE_ROOT):]
         else: raise Exception("Given subfolder path is not in either data root.")
+    else: ITEM = ""
+except Exception as e: print(e); sys.exit(2)
 
-    # validate existence of item to compare
-    print(f"Trying to compare:\n{LEFT_ROOT+ITEM}\n{RITE_ROOT+ITEM}\n")
+# validate existence of item to compare
+try:
+    print(f"\nTrying to compare:\n{LEFT_ROOT+ITEM}\n{RITE_ROOT+ITEM}\n")
     if not (os.path.exists(LEFT_ROOT+ITEM) and os.path.exists(RITE_ROOT+ITEM)): raise Exception("One of the items above was not found.")
-
-except Exception as e: print(f"Input parameters error: {e}"); sys.exit(2)
+except Exception as e: print(e); sys.exit(2)
 
 # perform comparison
 try: 
@@ -82,7 +113,12 @@ try:
     if os.name == 'posix':  unix_compare()
 
     # log
-    with open(LOG_FILE,'a') as log: log.write(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M')} ({platform.node()})  /{ITEM}\n")
+    if LOG_FILE is not None:
+        try: 
+            with open(LOG_FILE,'a') as log: 
+                log.write(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M')} ({platform.node()})  {'\\' if os.name == 'nt' else '/'}{ITEM}\n")
+        except Exception as e: print(f"Operation was not logged: {e}")
+
 except Exception as e: print(f"Error during comparison: {e}"); sys.exit(1)
 
 sys.exit(0)
